89a5e90a2df9584ba92b3e32fcb0e1c4c81e6be8
Samuli Kohomäki <samuli.kohomaki@gmail.com>
2017-03-07 17:21:02 +0200

    Added models and validators

---
+++ app/app/models.py
@@ -0,0 +1,51 @@
+from django.db import models
+from django.core.exceptions import ValidationError
+from app.validators import *
+
+
+class Serie(models.Model):
+    name = models.CharField(primary_key=True, max_length=64)
+
+class Season(models.Model):
+    year = models.IntegerField(null=False, blank=False, validators=[greaterThanZero])
+    serie = models.ForeignKey(Serie, related_name="seasons", null=False, blank=False)
+
+class Team(models.Model):
+    name = models.CharField(primary_key=True, max_length=128)
+
+class SeasonTeam(Team):
+    season = models.ForeignKey(Season, related_name="teams", null=False, blank=False)
+
+class Driver(models.Model):
+    name = models.CharField(primary_key=True, max_length=256)
+    carNumber = models.IntegerField(validators=[greaterThanZero])
+
+class SeasonDriver(Driver):
+    team = models.ForeignKey(SeasonTeam, related_name="drivers", null=False, blank=False)
+
+class Round(models.Model):
+    roundNumber = models.IntegerField(null=False, blank=False, validators=[greaterThanZero])
+    roundName = models.CharField(null=False, blank=False, max_length=256)
+    season = models.ForeignKey(Season, related_name="rounds", null=False, blank=False)
+
+class Competition(models.Model):
+    season = models.ForeignKey(Season, related_name="competitions", null=False, blank=False)
+
+class Player(models.Model):
+    name = models.CharField(primary_key=True, max_length=256)
+    points = models.IntegerField(null=False, blank=False, validators=[greaterThanZero])
+    competition = models.ForeignKey(Competition, related_name="players", null=False, blank=False)
+
+class Row(models.Model):
+    QUALIFICATION = "Q"
+    RACE = "R"
+    ROW_CHOICES = (
+        (QUALIFICATION, "qualification"),
+        (RACE, "race"),
+    )
+
+    row = models.CharField(null=False, blank=False, validators=[rowLengthIsCorrect], max_length=64)
+    rowType = models.CharField(null=False, blank=False, choices = ROW_CHOICES, default=RACE, max_length=1)
+
+class PlayerRow(Row):
+    player = models.ForeignKey(Player, related_name="playerRows", null=False, blank=False)
---
+++ app/app/validators.py
@@ -0,0 +1,15 @@
+from django.core.exceptions import ValidationError
+from django.utils.translation import ugettext_lazy
+from app.models import *
+
+
+def greaterThanZero(value):
+    if value <= 0:
+        raise ValidationError("Value of integer must be greater than zero!")
+
+def rowLengthIsCorrect(value):
+    rowLength = Season.teams.all().count()
+    if value != rowLength:
+        raise ValidationError(
+            ugettext_lazy("Row length was incorrect, it must be %(value)s"),
+            params={'value': rowLength})
